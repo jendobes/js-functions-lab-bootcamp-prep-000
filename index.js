842c148e7d55278b2a151714de4e1c656d98898c
Jennifer Dobak <jdobak1@gmail.com>
2017-10-03 17:45:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 function happyHolidays() {
-  console.log("Happy Holidays!")
+  return `Happy Holidays!`
 }
 function happyHolidaysTo(name){
-  console.log(`Happy holidays, ${name}!`)
+  return `Happy holidays, ${name}!`
 }
 function happyHolidayTo(holiday, name) {
-  console.log(`Happy ${holiday}, ${name}!`)
+  return `Happy ${holiday}, ${name}!`
 }
 function holidayCountdown(days, holiday) {
-  console.log(`It's ${days} days unitl ${holiday}!`)
+  return `It's ${days} days unitl ${holiday}!`
 }
